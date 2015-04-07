b468796227670e722e6a1fa4f7f318328e95b09b
Gustavo Muro <gustmuro@gmail.com>
2015-04-06 22:15:11 -0300

    se elimino la configuracion del cape

--- adc/main.c
+++ adc/main.c
@@ -10,30 +10,23 @@
 #include <unistd.h>
 
 /*==================[macros and definitions]=================================*/
-#define AIN0_PATH          "/sys/bus/iio/devices/iio:device0/in_voltage3_raw"
-#define AIN0_STR_LENGTH    10
+#define AIN3_PATH          "/sys/bus/iio/devices/iio:device0/in_voltage3_raw"
+#define AIN3_STR_LENGTH    10
 
-#define SLOTS_PATH   "/sys/devices/bone_capemgr.9/slots"
-#define DATAW_SLOTS  "cape-bone-iio"
 /*==================[external functions definition]==========================*/
 
 int main(void)
 {
-   FILE *ain0Handle;
-   FILE *slotsHandle;
-   char str[AIN0_STR_LENGTH];
-
-   slotsHandle = fopen(SLOTS_PATH, "w");
-   fwrite(DATAW_SLOTS, sizeof(DATAW_SLOTS), 1, slotsHandle);
-   fclose(slotsHandle);
+   FILE *ain3Handle;
+   char str[AIN3_STR_LENGTH];
 
    while (1)
    {
-      ain0Handle = fopen(AIN0_PATH, "r");
-      fread(str, AIN0_STR_LENGTH, 1, ain0Handle);
-      fclose(ain0Handle);
+      ain3Handle = fopen(AIN3_PATH, "r");
+      fread(str, AIN3_STR_LENGTH, 1, ain3Handle);
+      fclose(ain3Handle);
 
-      printf("AIN=%s\r",str);
+      printf("AIN3=%s\r",str);
       usleep(100000);
    }
 
